3928b122b2422192d1f5e6b0a1848814af8f0421
apoorvkh <mail@apoorvkh.com>
2023-06-04 20:44:53 -0400

    Correcting setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     name="viper",
     description="Implementation of ViperGPT",
     packages=['viper'],
-    package_data={'viper': ['data/**/*.txt', 'data/**/*.json']}
+    package_data={'viper': ['data/**/*.txt', 'data/**/*.json']},
     install_requires=read_requirements("requirements.txt"),
     python_requires=">=3.7",
 )
