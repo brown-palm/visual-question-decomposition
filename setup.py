375f59e3b64b1befa546350c02f1b253e3a2152d
apoorvkh <mail@apoorvkh.com>
2023-06-05 03:28:57 -0400

    Fixed requirements

--- setup.py
+++ setup.py
@@ -3,26 +3,11 @@ from setuptools import setup
 
 def read_requirements(filename: str):
     with open(filename) as requirements_file:
-        # import re
-
-        # def fix_url_dependencies(req: str) -> str:
-        #     """Pip and setuptools disagree about how URL dependencies should be handled."""
-        #     m = re.match(
-        #         r"^(git\+)?(https|ssh)://(git@)?github\.com/([\w-]+)/(?P<name>[\w-]+)\.git(@([\w-]+))?(#egg=(?P<package_name>[\w-]+))?",
-        #         req
-        #     )
-        #     if m is None:
-        #         return req
-        #     else:
-        #         package_name = m.group('package_name') or m.group('name')
-        #         return f"{package_name} @ {req}"
-
         requirements = []
         for line in requirements_file:
             line = line.strip()
             if line.startswith("#") or len(line) <= 0:
                 continue
-            # requirements.append(fix_url_dependencies(line))
             requirements.append(line)
 
     return requirements
